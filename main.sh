26e6b7c19aff560b7a21aa4b9945c0620afb0dc1
Christopher Griffis <40696844+chdgriff@users.noreply.github.com>
2024-10-06 18:37:05 -0700

    added default apikey and fixed gtn_addr

--- main.sh
+++ main.sh
@@ -5,8 +5,9 @@ qbt_username="${QBT_USERNAME:-admin}"
 qbt_password="${QBT_PASSWORD:-adminadmin}"
 qbt_addr="${QBT_ADDR:-http://localhost:8080}" # ex. http://10.0.1.48:8080
 gtn_addr="${GTN_ADDR:-http://localhost:8000}" # ex. http://10.0.1.48:8000
+gtn_apikey="${GTN_APIKEY:-CHANGEME}"
 
-port_number=$(curl --fail --silent --show-error --header "X-API-Key: $GTN_APIKEY" $GTN_ADDR/v1/openvpn/portforwarded | jq '.port')
+port_number=$(curl --fail --silent --show-error --header "X-API-Key: $GTN_APIKEY" $gtn_addr/v1/openvpn/portforwarded | jq '.port')
 if [ ! "$port_number" ] || [ "$port_number" = "0" ]; then
     echo "Could not get current forwarded port from gluetun, exiting..."
     exit 1
